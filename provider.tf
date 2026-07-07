3adbd713395554a1c37e9ebbe2c52bda03ad91ee
Miguel Andreu <miguelandreu@MacBook-Air-de-Miguel.local>
2026-07-07 14:02:43 +0200

    flows

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "1.15.6"
+
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  use_oidc = true
+}
